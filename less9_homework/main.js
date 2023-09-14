68f8a0b9a4f7c8a25a92c04af23f087085be22bf
Vlados <vladislav25012008@gmail.com>
2023-09-14 19:27:45 +0300

    lessons homework

--- less9_homework/main.js
+++ less9_homework/main.js
@@ -100,4 +100,194 @@ for (let i = 0; i < coursesAndDurationArray.length; i++) {
 document.body.appendChild(container);
 
 
+//5 - є масив simpsons, котрий лежить в arrays.js (на цей момент ви вже знаєте де він знаходиться)
+// Проітерувати його, створиши для кожного об'єкту  масиву <div class='member'> та наповнити його данними з об'єкту.
+//     Якщо людською мовою: під кожного члена родини зробити блок та наповнити його інформацією з цього об'єкту
+//
+// =========================
+let simpsons = [
+    {
+        name: 'Bart',
+        surname: 'Simpson',
+        age: 10,
+        info: 'Бартолом\'ю ДжоДжо «Барт» Сімпсон (англ. Bartholomew JoJo «Bart» Simpson) — один із головних героїв мультиплікаційного серіалу Сімпсони. Барт — найстарша дитина Гомера і Мардж Сімпсон. У нього також є дві молодші сестри — Ліса і Меґґі. Барт є втіленням образу бешкетника та посереднього учня у школі. Разом зі своїм батьком Барт є одним із найвідоміших персонажів у цьому серіалі.',
+        photo: 'https://upload.wikimedia.org/wikipedia/uk/a/aa/Bart_simpson.png'
+    },
+    {
+        name: 'Homer',
+        surname: 'Simpson',
+        age: 40,
+        info: 'Гомер Джей Сімпсон (англ. Homer Jay Simpson) — один із головних героїв мультсеріалу «Сімпсони». Гомер — грубий і неввічливий батько родини, він має очевидні вади: товстий, лисий і не дуже розумний. Нерідко він поводиться як блазень, абсурдно, егоїстично і нетактовно, але все ж лишається симпатичним.',
+        photo: 'http://upload.wikimedia.org/wikipedia/en/0/02/Homer_Simpson_2006.png'
+    },
+    {
+        name: 'Marge',
+        surname: 'Simpson',
+        age: 38,
+        info: 'Ма́рджори Жакли́н «Мардж» Си́мпсон (в девичестве Бувье́) (англ. Marjorie Jacqueline «Marge» Simpson) — постоянный персонаж мультипликационного сериала «Симпсоны», её озвучивает Джулия Кавнер. Обычно носит зелёное платье, красные балетки, на шее — ожерелье из искусственного жемчуга и ездит на оранжевом универсале. У неё шикарные синие волосы, которые она обычно собирает в очень высокую причёску. Глаза цвета ореха (19s6e). Основное занятие — домохозяйка, большую часть времени проводит в заботе о доме, детях и Гомере. Образ Мардж копирует стереотип провинциальной американской домохозяйки 50-х годов. Мардж — единственный член семьи, посещающий церковь добровольно. Старается поддерживать нравственность не только своей семьи, но и всего города. Отлично готовит, особенно славятся её свиные отбивные и зефир. Любимое блюдо — лапша с маслом.',
+        photo: 'https://upload.wikimedia.org/wikipedia/ru/0/0b/Marge_Simpson.png'
+    },
+    {
+        name: 'Lisa',
+        surname: 'Simpson',
+        age: 9,
+        info: 'Ли́за Мари́ Си́мпсон (англ. Lisa Marie Simpson) — героиня мультипликационного сериала «Симпсоны». Средний ребёнок в семье, восьмилетняя девочка, выделяющаяся среди остальных Симпсонов прежде всего своим умом и рассудительностью.',
+        photo: 'https://upload.wikimedia.org/wikipedia/ru/e/ec/Lisa_Simpson.png'
+    },
+    {
+        name: 'Maggie',
+        surname: 'Simpson',
+        age: 1,
+        info: 'Ма́ргарет Эвелин «Мэ́гги» Си́мпсон (англ. Margaret Evelyn “Maggie” Simpson) — персонаж мультсериала «Симпсоны». Впервые появилась на телевидении в шоу Трейси Ульман, в короткометражке Good Night (англ.)русск. 19 апреля 1987 года. Мэгги была придумана и разработана карикатуристом Мэттом Грейнингом, пока он ждал встречи с Джеймсом Л. Бруксом. Названа в честь младшей сестры Грейнинга. После появления в шоу Трейси Ульман, через три года семья Симпсонов получила собственный сериал на телеканале Fox, дебют произошёл 17 декабря 1989 года.',
+        photo: 'https://upload.wikimedia.org/wikipedia/ru/9/9d/Maggie_Simpson.png'
+    },
+];
+
+for (let i = 0; i < simpsons.length; i++) {
+    let simpsonObj = document.createElement("div")
+    simpsonObj.classList.add('member')
+
+    let simpsonName = document.createElement('h1')
+    simpsonName.innerText = simpsons[i].name;
+
+    let simpsonSurname = document.createElement("h2");
+    simpsonSurname.innerText = simpsons[i].surname;
+
+    let simpsonAge = document.createElement("p");
+    simpsonAge.innerText = `Age: ${simpsons[i].age}`;
+
+    let simpsonInfo = document.createElement("p");
+    simpsonInfo.innerText = simpsons[i].info;
+
+    let simpsonPhoto = document.createElement("img");
+    simpsonPhoto.src = simpsons[i].photo;
+
+    simpsonObj.appendChild(simpsonName)
+    simpsonObj.appendChild(simpsonSurname)
+    simpsonObj.appendChild(simpsonAge)
+    simpsonObj.appendChild(simpsonInfo)
+    simpsonObj.appendChild(simpsonPhoto)
+
+    document.body.appendChild(simpsonObj)
+}
+
+//6 Цикл в циклі
+// - Є масив coursesArray котрий лежить в arrays.js (на цей момент ви вже знаєте де він знаходиться)
+//
+// Створити для кожного елементу масиву свій блок, блок розділити блоками, в яких будуть зберігатись значення окремих властивостей, для властивості modules зробити список з елементами
+// Приклад структири знаходиться у файлі example.png який лежить в папці з поточним фйлом
+// ------------------
+let coursesArray = [
+    {
+        title: 'JavaScript Complex',
+        monthDuration: 5,
+        hourDuration: 909,
+        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'node.js']
+    },
+    {
+        title: 'Java Complex',
+        monthDuration: 6,
+        hourDuration: 909,
+        modules: ['html',
+            'css',
+            'js',
+            'mysql',
+            'mongodb',
+            'angular',
+            'aws',
+            'docker',
+            'git',
+            'java core',
+            'java advanced']
+    },
+    {
+        title: 'Python Complex',
+        monthDuration: 6,
+        hourDuration: 909,
+        modules: ['html',
+            'css',
+            'js',
+            'mysql',
+            'mongodb',
+            'angular',
+            'aws',
+            'docker',
+            'python core',
+            'python advanced']
+    },
+    {
+        title: 'QA Complex',
+        monthDuration: 4,
+        hourDuration: 909,
+        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'git', 'QA/QC']
+    },
+    {
+        title: 'FullStack',
+        monthDuration: 7,
+        hourDuration: 909,
+        modules: ['html',
+            'css',
+            'js',
+            'mysql',
+            'mongodb',
+            'react',
+            'angular',
+            'aws',
+            'docker',
+            'git',
+            'node.js',
+            'python',
+            'java']
+    },
+    {
+        title: 'Frontend',
+        monthDuration: 4,
+        hourDuration: 909,
+        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'sass']
+    }
+];
+// {
+//     title: 'JavaScript Complex',
+//         monthDuration: 5,
+//     hourDuration: 909,
+//     modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'node.js']
+// },
+let fatherDiv = document.createElement("div")
+fatherDiv.classList.add('father')
+let headDiv = document.createElement('div')
+headDiv.classList.add('head')
+let mainDiv = document.createElement("div")
+mainDiv.classList.add('main')
+let main1 = document.createElement("div")
+let main2 = document.createElement("div")
+main1.classList.add('main1')
+main2.classList.add('main2')
+let listDiv = document.createElement("div")
+listDiv.classList.add('listDiv')
+let list = document.createElement("ul")
+list.classList.add('list')
+fatherDiv.append(headDiv, mainDiv, listDiv)
+mainDiv.append(main1, main2)
+listDiv.append(list)
+document.body.appendChild(fatherDiv)
+        let course = coursesArray[1];
+
+        let titleDiv = document.createElement("h1");
+        titleDiv.innerText = course.title;
+
+        let monthDiv = document.createElement("h2");
+        monthDiv.innerText = course.monthDuration;
+
+        let hourDiv = document.createElement("h2");
+        hourDiv.innerText = course.hourDuration;
+
+        let listItem = document.createElement('li');
+        listItem.innerText = course.modules.join(', ');
+
+    headDiv.appendChild(titleDiv);
+    main1.appendChild(monthDiv);
+    main2.appendChild(hourDiv);
+    list.appendChild(listItem);
+
+
 
